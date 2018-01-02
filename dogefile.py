6300df7585c3c7d3392a48107e34d079d0fa8d09
Kirill Sulim <kirillsulim@gmail.com>
2018-01-02 14:31:40 +0500

    WIP

--- dogefile.py
+++ dogefile.py
@@ -3,7 +3,8 @@ from dogebuild.Doge import Doge
 
 d = Doge()
 fpc = d.use_plugin("fpc")
-fpc.dir = "./src/main.pas"
+fpc.main_file = "./src/main.pas"
+fpc.main_test_file = "./test/test.pas"
 
 d.build()
 
